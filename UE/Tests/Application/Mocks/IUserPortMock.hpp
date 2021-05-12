f5266de1777163146c372b3134a3a0aa06484d5b
PawelSli <44865813+PawelSli@users.noreply.github.com>
2021-05-12 17:59:31 +0200

    Check1

--- UE/Tests/Application/Mocks/IUserPortMock.hpp
+++ UE/Tests/Application/Mocks/IUserPortMock.hpp
@@ -26,10 +26,6 @@ public:
     MOCK_METHOD(void, showNotConnected, (), (final));
     MOCK_METHOD(void, showConnecting, (), (final));
     MOCK_METHOD(void, showConnected, (), (final));
-    MOCK_METHOD(void, showCallRequest, (common::PhoneNumber phoneNumber), (final));
-    MOCK_METHOD(void, talk, (common::PhoneNumber phoneNumber), (final));
-    MOCK_METHOD(void, showPartnerNotAvailable, (common::PhoneNumber phoneNumber), (final));
-    MOCK_METHOD(void, showStartMenu,(), (final));
 };
 
 }
