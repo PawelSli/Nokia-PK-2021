696e355e984b41395c81b80fda6e19e6ed847d12
PawelSli <44865813+PawelSli@users.noreply.github.com>
2021-05-12 18:17:06 +0200

    Update BtsPort.hpp

--- UE/Application/Ports/BtsPort.hpp
+++ UE/Application/Ports/BtsPort.hpp
@@ -16,8 +16,6 @@ public:
     void stop();
 
     void sendAttachRequest(common::BtsId) override;
-    void sendCallAccept(common::PhoneNumber) override;
-    void sendCallDrop(common::PhoneNumber) override;
 
 private:
     void handleDisconnected();
