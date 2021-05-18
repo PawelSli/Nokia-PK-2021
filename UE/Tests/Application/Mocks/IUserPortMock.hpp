bd88d760e48b2f4e79e8e66532e08e4019b9a4e9
PawelS <ps439787@gmail.com>
2021-05-18 20:55:14 +0200

    Modifying UserPortMock for sending-call-request

--- UE/Tests/Application/Mocks/IUserPortMock.hpp
+++ UE/Tests/Application/Mocks/IUserPortMock.hpp
@@ -12,8 +12,10 @@ public:
     IUserEventsHandlerMock();
     ~IUserEventsHandlerMock() override;
 
-    MOCK_METHOD(void, handleCallAccepted, (), (final));
-    MOCK_METHOD(void, handleCallRejected, (), (final));
+    MOCK_METHOD(void, USER_handleCallAccept, (common::PhoneNumber), (final));
+    MOCK_METHOD(void, USER_handleStartDial, (), (final));
+    MOCK_METHOD(void, USER_handleCallRequest, (common::PhoneNumber phoneNumber), (final));
+    MOCK_METHOD(void, USER_handleCallDrop, (common::PhoneNumber), (final));
 
 };
 
@@ -23,13 +25,16 @@ public:
     IUserPortMock();
     ~IUserPortMock() override;
 
-    MOCK_METHOD(void, showNotConnected, (), (final));
-    MOCK_METHOD(void, showConnecting, (), (final));
-    MOCK_METHOD(void, showConnected, (), (final));
-    MOCK_METHOD(void, showCallRequest, (common::PhoneNumber phoneNumber), (final));
-    MOCK_METHOD(void, talk, (common::PhoneNumber phoneNumber), (final));
-    MOCK_METHOD(void, showPartnerNotAvailable, (common::PhoneNumber phoneNumber), (final));
-    MOCK_METHOD(void, showStartMenu,(), (final));
+    MOCK_METHOD(void, USER_showNotConnected, (), (final));
+    MOCK_METHOD(void, USER_showConnecting, (), (final));
+    MOCK_METHOD(void, USER_showConnected, (), (final));
+    MOCK_METHOD(void, USER_showCallRequest, (common::PhoneNumber phoneNumber), (final));
+    MOCK_METHOD(void, USER_callAchieved, (common::PhoneNumber phoneNumber), (final));
+    MOCK_METHOD(void, USER_showPartnerNotAvailable, (common::PhoneNumber phoneNumber), (final));
+    MOCK_METHOD(void, USER_startTalking, (common::PhoneNumber phoneNumber), (final));
+    MOCK_METHOD(void, USER_showStartMenu,(), (final));
+    MOCK_METHOD(void, USER_showEnterPhoneNumber, (), (final));
+    MOCK_METHOD(void, USER_showDialing, (common::PhoneNumber phoneNumber), (final));
 };
 
 }
